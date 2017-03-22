f164dd8eb57931b5433d9558ec1827af39f9f496
Rosie Gao <yumeng1@ualberta.ca>
2017-03-22 01:23:58 -0600

    save changes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -17,16 +17,31 @@ class PageController extends Controller
 
         $page= $this->getIndexPage($project_id);
 
+        $this->createLocalPageFile($page);
+
         return redirect() -> route('page.home', [ 'project_id' => $project_id, 'page_name' => $page->name ]);
     }
 
     protected function insertToPageTable($project_id){
+        $id = \DB::table('project_pages')->where('project_id', $project_id)->get()->count();
+        $name_format = 'untitled%d';
+        $name = sprintf($name_format, $id+1);
+
         return \DB::table('project_pages')->insert(
-            ['project_id' => $project_id, 'name' => 'untitled', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')]
+            [
+                'project_id' => $project_id,
+                'name' => $name,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
         );
     }
 
     protected function getIndexPage($project_id){
         return \DB::table('project_pages')->where('project_id', $project_id)->orderBy('created_at', 'desc')->first();
+    }
+
+    protected function createLocalPageFile($page){
+
+        return '';
     }
 }
--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use phpDocumentor\Reflection\Types\String_;
 use Ramsey\Uuid\Uuid;
 
 class ProjectController extends Controller
@@ -19,16 +20,36 @@ class ProjectController extends Controller
         $uuid = Uuid::uuid1();
         $this->insertToProjectTable($user, $uuid);
         $project = $this->getLatestProject($user);
+        $this->createLocalProjectFolder($project);
         return redirect() -> route('project.home', [ 'project_id' => $project->id]);
     }
 
     protected function insertToProjectTable($user, $uuid){
+        $id = \DB::table('projects')->get()->count();
+        $name_format = 'untitled%d';
+        $name = sprintf($name_format, $id+1);
+
+        $relative_path = '456\456';
+        $path_format = '%s\%s';
+        $storage_path = sprintf($path_format, $relative_path, $name);
+
         return \DB::table('projects')->insert(
-            ['id' => $uuid, 'user_id' => $user->id, 'name' => 'untitled', 'created_at' => Carbon::now()->format('Y-m-d H:i:s')]
+            [
+                'id' => $uuid,
+                'user_id' => $user->id,
+                'name' => $name,
+                'storage_path' => $storage_path,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
         );
     }
 
     protected function getLatestProject($user){
         return \DB::table('projects')->where('user_id', $user->id)->orderBy('created_at', 'desc')->first();
+    }
+
+    protected function createLocalProjectFolder($project){
+
+        return '';
     }
 }
